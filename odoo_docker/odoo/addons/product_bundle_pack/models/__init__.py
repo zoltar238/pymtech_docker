a53998c99d4ee9f90cefe4c3b4d629b0d08fc1c1
Andrei <protoandrei@protonmail.com>
2025-05-12 19:17:09 +0200

    Actualizaciones

--- odoo_docker/odoo/addons/product_bundle_pack/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-from . import sale
-from . import product
-from . import purchase
-
--- odoo_docker/odoo/addons/ps_ticket_rel_almacen/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import helpdeskticket
-from . import saleorder
-from . import saleorderline
